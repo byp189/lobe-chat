ea2643b8db90650356cdbc174c09e6fa290dc823
byp189 <baoyipen@gmail.com>
2025-12-04 21:42:16 +0800

    Update metadata.ts

--- src/locales/default/metadata.ts
+++ src/locales/default/metadata.ts
@@ -1,33 +1,33 @@
 export default {
   changelog: {
-    description: '持续追踪 {{appName}} 的新功能和改进',
-    title: '更新日志',
+    description: 'ABEI专用AI',
+    title: 'ABEI专用AI',
   },
   chat: {
-    description: '{{appName}} 带给你最好的 ChatGPT, Claude , Gemini, OLLaMA WebUI 使用体验',
-    title: '{{appName}}：个人 AI 效能工具，给自己一个更聪明的大脑',
+    description: 'ABEI专用AI',
+    title: 'ABEI专用AI',
   },
   discover: {
     assistants: {
       description:
-        '内容创作、文案、问答、图像生成、视频生成、语音生成、智能 Agent、自动化工作流，定制你专属的 AI / GPTs / OLLaMA 智能助手',
-      title: '智能体市场',
+        'ABEI专用AI',
+      title: 'ABEI专用AI 智能体市场',
     },
     description:
-      '内容创作、文案、问答、图像生成、视频生成、语音生成、智能 Agent、自动化工作流、自定义AI应用，定制你专属的 AI 应用工作台',
+      '内容创作、文案、问答、图像生成、视频生成、语音生成、智能 Agent、自动化工作流、自定义AI应用，定制专属的 AI 应用工作台',
     mcp: {
       description:
         '搜索、比较并连接数千个 MCP Servers，帮助 AI 系统轻松访问文件系统、数据库、API 等关键资源，全面扩展你的 AI 能力',
-      title: 'MCP Servers 市场',
+      title: 'ABEI专用AI MCP Servers 市场',
     },
     models: {
-      description: '探索主流 AI 模型 OpenAI / GPT / Claude 3 / Gemini / Ollama / Azure / DeepSeek',
-      title: '模型市场',
+      description: 'ABEI专用AI',
+      title: 'ABEI专用AI 模型市场',
     },
     plugins: {
       description:
         '搜素图表生成、学术、图像生成、视频生成、语音生成、自动化工作流，为你的助手集成丰富的插件能力',
-      title: '插件市场',
+      title: 'ABEI专用AI 插件市场',
     },
     providers: {
       description:
@@ -38,7 +38,7 @@ export default {
     title: '发现',
   },
   image: {
-    description: '{{appName}} 带给你最好的 GPT Image, Flux, Midjourney, Stable Diffusion 使用体验',
+    description: 'ABEI专用AI 带给你最好的 GPT Image, Flux, Midjourney, Stable Diffusion 使用体验',
     title: 'AI 绘画',
   },
   plugins: {
@@ -47,7 +47,7 @@ export default {
     title: '插件市场',
   },
   welcome: {
-    description: '{{appName}} 带给你最好的 ChatGPT, Claude , Gemini, OLLaMA WebUI 使用体验',
-    title: '欢迎使用 {{appName}}：个人 AI 效能工具，给自己一个更聪明的大脑',
+    description: 'ABEI专用AI',
+    title: 'ABEI专用AI
   },
 };
