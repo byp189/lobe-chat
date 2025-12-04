015f2a49a4840e1bbea779840e1fb7c9c74dbe90
byp189 <baoyipen@gmail.com>
2025-12-04 20:12:27 +0800

    Fix branding and organization constants

--- packages/const/src/version.ts
+++ packages/const/src/version.ts
@@ -7,6 +7,6 @@ export const CURRENT_VERSION = pkg.version;
 export const isDesktop = process.env.NEXT_PUBLIC_IS_DESKTOP_APP === '1';
 
 // @ts-ignore
-export const isCustomBranding = BRANDING_NAME = false;
+export const isCustomBranding = false;
 // @ts-ignore
-export const isCustomORG = ORG_NAME !== 'LobeHub';
+export const isCustomORG = false;
