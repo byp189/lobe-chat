d9169cb58a95b1eb93f0ddd2b949181024138eac
byp189 <baoyipen@gmail.com>
2025-12-04 21:14:45 +0800

    Update branding.ts

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -2,12 +2,12 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'ABEI';
+export const LOBE_CHAT_CLOUD = 'ABEI AI';
 
-export const BRANDING_NAME = 'ABEI';
+export const BRANDING_NAME = 'ABEI AI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'ABEI';
+export const ORG_NAME = 'ABEI AI';
 
 export const BRANDING_URL = {
   help: undefined,
