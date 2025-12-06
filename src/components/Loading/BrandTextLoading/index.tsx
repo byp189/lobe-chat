a0fe198777837d874299c203266f43b59ba62b7e
byp <baoyipen@gmail.com>
2025-12-06 15:43:29 +0800

    222

--- src/components/Loading/BrandTextLoading/index.tsx
+++ src/components/Loading/BrandTextLoading/index.tsx
@@ -5,12 +5,18 @@ import { isCustomBranding } from '@/const/version';
 
 import CircleLoading from '../CircleLoading';
 
+// 定义一个新的 React 组件来渲染品牌文本
+const ABEI_AI_Text = () => {
+  return <>ABEI AI Welcome You</>;
+};
+
 export default () => {
   if (isCustomBranding) return <CircleLoading />;
 
   return (
     <Center height={'100%'} width={'100%'}>
-      <BrandLoading size={40} style={{ opacity: 0.6 }} text="ABEI AI Welcom You" />
+      {/* 将 text 属性的值从字符串替换为我们定义的 ABEI_AI_Text 组件 */}
+      <BrandLoading size={40} style={{ opacity: 0.6 }} text={ABEI_AI_Text} />
     </Center>
   );
 };
