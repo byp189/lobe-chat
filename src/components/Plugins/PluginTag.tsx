d97bf3ca41d8a259d5503377b9b7447a90f3ea48
byp <baoyipen@gmail.com>
2025-12-07 23:54:06 +0800

    33

--- src/components/Plugins/PluginTag.tsx
+++ src/components/Plugins/PluginTag.tsx
@@ -17,7 +17,7 @@ const PluginTag = memo<PluginTagProps>(
   ({ showIcon = true, author, type, showText = true, isMCP }) => {
     const { t } = useTranslation('plugin');
     const isCustom = type === 'customPlugin';
-    const isOfficial = author === 'ABEI AI FROM OFFICIAL';
+    const isOfficial = author === 'OFFICIAL';
 
     const customTag = (
       <Tag color={'warning'} icon={showIcon && <Icon icon={Package} />} size={'small'}>
