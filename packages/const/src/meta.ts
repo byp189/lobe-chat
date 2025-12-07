9b45533663d6184874697ce9a6e389f4f261deb1
byp <baoyipen@gmail.com>
2025-12-07 18:05:44 +0800

    touxiang

--- packages/const/src/meta.ts
+++ packages/const/src/meta.ts
@@ -2,11 +2,11 @@ import { MetaData } from '@lobechat/types';
 
 import { BRANDING_LOGO_URL } from './branding';
 
-export const DEFAULT_AVATAR = '🤖';
+export const DEFAULT_AVATAR = '/1f92f.webp';
 export const DEFAULT_USER_AVATAR = '😀';
 export const DEFAULT_SUPERVISOR_AVATAR = '🎙️';
 export const DEFAULT_SUPERVISOR_ID = 'supervisor';
 export const DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)';
 export const DEFAULT_AGENT_META: MetaData = {};
-export const DEFAULT_INBOX_AVATAR = BRANDING_LOGO_URL || '🤯';
+export const DEFAULT_INBOX_AVATAR = BRANDING_LOGO_URL || '/1f92f.webp';
 export const DEFAULT_USER_AVATAR_URL = BRANDING_LOGO_URL || '/icons/icon-192x192.png';
